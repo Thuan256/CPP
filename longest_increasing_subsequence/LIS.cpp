1ea5ec0fa1a36a4c095a0dd8499ce4abd45ff9df
mthunz <fakemggg@gmaill.com>
2024-11-15 06:53:04 +0700

    auto

---
+++ longest_increasing_subsequence/LIS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+    ifstream inp("LIS.inp");
+    string line;
+    
+    getline(inp, line);
+    short n = stoi(line);
+
+    stringstream ss(line);
+
+    vector
+
+
+    return 0;
+}

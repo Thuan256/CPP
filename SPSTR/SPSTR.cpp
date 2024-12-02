c04050bffcd71eafda68e88ba4210b437ad23cca
mthunz <fakemggg@gmaill.com>
2024-12-03 06:46:37 +0700

    auto

--- SPSTR/SPSTR.cpp
+++ SPSTR/SPSTR.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-
     ifstream inp("SPSTR.inp");
 
     string str;
@@ -16,7 +15,6 @@ int main() {
     int i = 0, c = 0;
 
     while (str.length() > prefix.length()) {
-
         prefix.push_back(str[i++]);
 
         if (str == prefix) {
@@ -24,24 +22,22 @@ int main() {
             break;
         }
 
-        if (!str.compare(str.length() - prefix.length(), prefix.length(), prefix)) {
-
-            string mid_range = str.substr(prefix.length(), str.length() - 2 * prefix.length());
+        if (!str.compare(str.length() - prefix.length(), prefix.length(),
+                         prefix)) {
+            string mid_range =
+                str.substr(prefix.length(), str.length() - 2 * prefix.length());
 
             if (mid_range.find(prefix) == string::npos) {
-
                 prefix = "None ";
                 break;
             } else {
                 if ((str.length() % 2) == (prefix.length() % 2)) {
-
-                    string mid = str.substr((str.length() - prefix.length()) / 2, prefix.length());
+                    string mid = str.substr(
+                        (str.length() - prefix.length()) / 2, prefix.length());
 
                     c += (c == 0);
                 }
             }
-
-
 
         } else {
             if (c) break;

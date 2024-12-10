bdb6526b6254e8d8a6ef0ec5a5a8d6b9a56a06d4
mthunz <fakemggg@gmaill.com>
2024-12-11 06:51:48 +0700

    'auto'

--- contest/HS/huynh_minh_thuan/BOMCHUM.cpp
+++ contest/HS/huynh_minh_thuan/BOMCHUM.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+vector<bool> eratosthenes(int n) {
+    vector<bool> primes(n + 1, true);
+
+    primes[0] = primes[1] = false;
+
+    for (int p = 2; p * p <= n; p++)
+        if (primes[p])
+            for (int i = p * p; i <= n; i += p)
+                primes[p] = false;
+
+    return primes;
+}
+
 int main() {
     ifstream inp("BOMCHUM.inp");
     ofstream out("BOMCHUM.out");
@@ -10,24 +23,36 @@ int main() {
     int n;
     inp >> n;
 
-    vector<int> a(n);
+    vector<ll> a(n);
+
+    vector<bool> sieves = eratosthenes(10e5);
 
     for (int i = 0; i < n; i ++) {
         inp >> a[i];
-        ll sum = 0;
-        for (int j = 1; j <= sqrt(a[i]); j++) {
-            if (a[i] % j == 0) {
 
-                ll num = a[i] / j;
+        ll x = a[i], sum = 1;
 
-                if (num == j)
-                    sum += j;
-                else
-                    sum += j + num;
+        for (int p = 2; i < sieves.size(); p++) {
+            if (sieves[p]) {
+                if (p * p > x) break;
+
+                if (x % p == 0) {
+                    ll power_sum = 1, pow = 1;
+
+                    while (x % p == 0) {
+                        x /= p;
+                        pow *= p;
+                        power_sum += pow;
+                    }
+
+                    sum *= power_sum;
+                }
             }
         }
 
+        if (x > 1) sum *= (x + 1);
         out << sum << " ";
+
     }
 
     out.close();

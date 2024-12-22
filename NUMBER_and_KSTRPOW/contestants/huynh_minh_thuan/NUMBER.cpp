db20ccc218e02aa785a56c2bf65c402a96303d1b
mthunz <fakemggg@gmaill.com>
2024-12-23 00:05:55 +0700

    'auto'

--- NUMBER_and_KSTRPOW/contestants/huynh_minh_thuan/NUMBER.cpp
+++ NUMBER_and_KSTRPOW/contestants/huynh_minh_thuan/NUMBER.cpp
@@ -11,6 +11,26 @@ int main() {
     inp >> n >> k;
 
     vector<ll> a(n);
+    map<int, int> r;
+    ll ans = 0;
 
-    for (size_t i = 0; i < n; i++) inp >> a[i];
+    for (size_t i = 0; i < n; i++) {
+        inp >> a[i];
+        r[a[i] % k]++;
+    }
+
+    ans += min(1, r[0]);
+
+    for (int i = 1; i <= k / 2; i++) {
+
+        if (i == k - i) {
+            ans += min(1, r[i]);
+        } else {
+            ans += max(r[i], r[k - i]);
+        }
+    }
+
+    out << ans;
+
+    return 0;
 }

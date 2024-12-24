11cef87e538ffedb3ef80b05cc6b71a0c4dbe8cd
Thuan256 <fakemggg@gmail.com>
2024-12-24 11:43:53 +0700

    'auto'

--- DOREMON/DOREMON.cpp
+++ DOREMON/DOREMON.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1001][2][1001];
+int dp[1001][2];
 
 int main() {
     ifstream inp("DOREMON.inp");
@@ -10,9 +10,20 @@ int main() {
     int n,k;
     inp >> n >> k;
 
-    dp[1][1][1] = dp[1][0][1] = 1;
+    dp[1][1] = dp[1][0] = 1;
 
+    for (int i = 2; i <= n; i++) {
+        dp[i][0] = dp[i - 1][1];
+        dp[i][1] = dp[i - 1][0] + dp[i - 1][1];
 
+        if (i > k) {
+            dp[i][1] -= dp[i - k - 1][1];
+        }
+    }
+
+    int ans = dp[n][0] + dp[n][1];
+
+    cout << ans;
 
     inp.close();
     out.close();

87b58f5e8301555ae486f6fa637e5df8fef38c2c
mthunz <fakemggg@gmaill.com>
2025-01-04 06:58:53 +0700

    'auto'

--- THPT_DANG_THAI_MAI/contestants/huynh_minh_thuan/LBRACKET.cpp
+++ THPT_DANG_THAI_MAI/contestants/huynh_minh_thuan/LBRACKET.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #define sync freopen(NAME".inp", "r", stdin); freopen(NAME".out", "w", stdout);
 
 string s;
-int last = -1, L = 1, R = 1, ans_L, ans_R, ans;
+int last = -1, L = 1, R = 1, ans_L, ans_R, ans, i = 1;
 pair<int, int> p;
 
 int main() {
@@ -25,34 +25,48 @@ int main() {
     string s;
     cin >> s;
 
-    while (R <= s.size()) {
+    while (L <= s.size()) {
 
-        if (s[R - 1] == ')') {
-            if (p.fi) p.se++;
-            else L = R;
-        } else p.fi++;
+        if (s[L - 1] == '(') {
+            p.fi++;
 
-        if (p.fi && p.se && p.fi == p.se) {
+            R = L + 1;
 
-            int _L = L;
-
-            if (L == last) {
-                _L = last;
-            } else last = R;
-
-            if (R - _L > ans) {
-                ans = R - _L + 1;
-                ans_L = _L;
-                ans_R = R;
+            while (R <= s.size() && s[R] == ')' && p.se <= p.fi) {
+                p.se++;
+                R++;
             }
-            p = {0, 0};
-            L = R;
 
+            cout << R - L + 1 << endl << L << " " << R << endl;
         }
-        R++;
+        L++;
+
+//        if (s[R - 1] == ')') {
+//            if (p.fi) p.se++;
+//            else L = R;
+//        } else p.fi++;
+//
+//        if (p.fi && p.se && p.fi == p.se) {
+//
+//            int _L = L;
+//
+//            if (L == last) {
+//                _L = last;
+//            } else last = R;
+//
+//            if (R - _L > ans) {
+//                ans = R - _L + 1;
+//                ans_L = _L;
+//                ans_R = R;
+//            }
+//            p = {0, 0};
+//            L = R;
+//
+//        }
+//        R++;
     }
 
-    cout << ans << endl << ans_L << " " << ans_R;
+//    cout << ans << endl << ans_L << " " << ans_R;
 
     return 0;
 }

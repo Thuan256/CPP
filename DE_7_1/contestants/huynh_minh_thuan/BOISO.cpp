0b27a954d745b22146e12e9d0fdd8fd6d4f7fef2
Thuan256 <fakemggg@gmail.com>
2025-01-07 20:37:17 +0700

    'auto'

---
+++ DE_7_1/contestants/huynh_minh_thuan/BOISO.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "BOISO"
+
+#define limit 1000007
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define ull unsigned long long
+#define FOD(i,b,a) for (int i = (int) b; i >= (int) a; i--)
+#define FOR(i,a,b) for (int i = (int) a; i <= (int) b; i++)
+#define speedup ios_base::sync_with_stdio(0); cin.tie(0);
+#define sync freopen(NAME".inp", "r", stdin); freopen(NAME".out", "w", stdout);
+
+ull n;
+
+int main() {
+
+    speedup sync
+
+    cin >> n;
+    cout << (n / 3ull) + (n / 5ull) - (n / 15ull);
+
+    return 0;
+}
+
